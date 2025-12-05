d81f2c683b5d3bb9febf4d76f9f625eedd9fa20d
chensong <chensong857763541@gmail.com>
2025-12-05 17:35:46 +0800

    添加环境变量文件

---
+++ env.js
@@ -0,0 +1,6 @@
+module.exports = {
+    SMTP_USER: 'xxxxxxxx@qq.com',  // 发送的邮箱
+    SMTP_PASS: 'xxxxxxxx',   // SMTP授权码
+	TO_USER: 'yyyyyyyy@qq.com', // 接收的邮箱（可以和发送的邮箱同一个邮箱，但会导致有同时有发生和接收的邮件记录）
+	
+};
